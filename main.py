75b5485f760fa78a4fd4994bc1eef7ac5f143548
Taiko2k <Taiko2k@users.noreply.github.com>
2022-03-24 11:07:05 +1300

    fix

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python3
 
-# Load Gtk
+# Avvie!
+
+# Copyright 2019 Taiko2k captain(dot)gxj(at)gmail.com
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Gdk', '4.0')
